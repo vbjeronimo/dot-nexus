04436d27580cc7f8df31d1b13354eb3c58028a31
Vitor Jeronimo <vitor.bently@hotmail.com>
2023-06-13 09:00:38 -0300

    WIP doing some refactoring

--- nexus/main.py
+++ nexus/main.py
@@ -6,28 +6,29 @@ import tomllib
 from pathlib import Path
 
 
-EXTENSIONS = {
-    "kitty": ".conf"
+FILE_EXTENSIONS = {
+    "kitty": ".conf",
+    "qtile": ".py",
 }
 
-MISSING_ARGS = "no_args"
+NEXUS_DIR = Path.home().joinpath(".config", "nexus")
+COMPONENTS_DIR = NEXUS_DIR.joinpath("components")
+PROFILES_DIR = NEXUS_DIR.joinpath("profiles")
 
 def get_parser() -> argparse.ArgumentParser:
-    description = "A centralized .dotfiles management system."
-    parser = argparse.ArgumentParser(prog="nexus", description=description)
+    description = "A centralized .dotfiles management system"
+    parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument("-l", "--list", action="store_true",
-                        help="list the nexus profiles available to use")
-    parser.add_argument("-p","--load", nargs="?", const=MISSING_ARGS,
-                        help="load a nexus profile (specified by profile name)")
-    parser.add_argument("-d", "--debug",action="store_true",
-                        help="turn on debug mode")
+    parser.add_argument("-p", "--load", help="load a nexus profile (specified by profile name)")
+    parser.add_argument("-l", "--list", action="store_true", help="list all profiles available")
+    parser.add_argument("-d", "--debug", action="store_true", help="enable debug mode")
 
     return parser
 
 
 def parse_args(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
+    logging.debug(f"Command-line args: {args}")
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -37,7 +38,7 @@ def parse_args(parser: argparse.ArgumentParser) -> None:
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
 
-    if args.list or args.load == MISSING_ARGS:
+    if args.list:
         list_profiles()
         sys.exit(0)
 
@@ -47,72 +48,64 @@ def parse_args(parser: argparse.ArgumentParser) -> None:
 
 
 def list_profiles() -> None:
-    nexus_dir = Path.home().joinpath(".config", "nexus")
-    profiles_dir = nexus_dir.joinpath("profiles")
+    profiles = [profile.stem for profile in PROFILES_DIR.iterdir()]
 
-    profiles = [file.stem for file in profiles_dir.iterdir()]
-
-    print("List of profiles available:")
-    for profile in profiles:
-        print(f"  - {profile}")
+    print("\nAvailable profiles:")
+    for profile_name in profiles:
+        print(f"  - {profile_name}")
 
 
 def load_profile(profile_name: str) -> None:
-    config_dir = Path.home().joinpath(".config")
-    nexus_dir = config_dir.joinpath("nexus")
-    components_dir = nexus_dir.joinpath("components")
-    profiles_dir = nexus_dir.joinpath("profiles")
+    profile_to_load = None
+    for profile in PROFILES_DIR.iterdir():
+        if profile.stem == profile_name:
+            profile_to_load = profile
+            break
 
-    profiles = [file.stem for file in profiles_dir.iterdir()]
+    if profile_to_load is None:
+        logging.error(f"Could not find profile '{profile_name}' in {PROFILES_DIR}.")
+        list_profiles()
+        return
 
-    if profile_name in profiles:
-        with open(profiles_dir.joinpath(f"{profile_name}.toml"), "rb") as toml_file:
-            profile = tomllib.load(toml_file)
+    with open(profile_to_load, "rb") as toml_file:
+        profile_contents = tomllib.load(toml_file)
 
-        components_to_update = profile["nexus"]["components"]
+    components_to_update = profile_contents["nexus"]["components"]
+    logging.debug(f"Profile '{profile_to_load.stem}' updates the following components: {components_to_update}")
 
-        for component in components_to_update:
-            component_content = components_dir.joinpath(component).read_text()
+    for component in components_to_update:
+        component_to_load = COMPONENTS_DIR.joinpath(component)
+        if not component_to_load.exists():
+            logging.error(f"Could not find component '{component_to_load.stem}' in {COMPONENTS_DIR}.")
+            sys.exit(1)
 
-            updated_component: list[str] = []
-            for line in component_content.split("\n"):
-                match = re.search("<<(.*)>>", line)
-                if match is None:
-                    updated_component.append(line)
-                    continue
+        component_contents = component_to_load.read_text()
 
-                keys = match.group(1).split(".")
+        logging.debug(f"Updating component '{component}'...")
 
-                nested_value = profile
-                for key in keys:
-                    if key in nested_value:
-                        nested_value = nested_value[key]
-                    else:
-                        nested_value = None
+        updated_component = []
+        for line in component_contents:
+            re_match = re.search("<<(.*)>>", line)
 
-                updated_line = re.sub("<<.*>>", str(nested_value), line)
-                updated_component.append(updated_line)
+            if re_match is None:
+                updated_component.append(line)
+                continue
 
-            output = "\n".join(updated_component)
+            keys = re_match.group(1)
+            print(f"keys: {keys}")
 
-            output_file = profile["nexus"]["generated_file_name"]
-            ext = EXTENSIONS[component]
-            generated_file = config_dir.joinpath(component, f"{output_file}{ext}")
-            generated_file.write_text(output)
-
-            print(f"Generated file at '{generated_file.absolute()}' for component '{component}'.")
-
-
-def setup_logger() -> None:
-    logging.basicConfig(
-        format="[%(levelname)s] %(funcName)s: %(message)s",
-        level=logging.WARNING,
-        stream=sys.stdout
-    )
+        # TODO: continue here...
 
 
 def main():
-    setup_logger()
+    logging.basicConfig(
+        format="[%(levelname)s] %(funcName)s: %(message)s",
+        level=logging.INFO,
+        stream=sys.stdout
+    )
+
+    COMPONENTS_DIR.mkdir(parents=True, exist_ok=True)
+    PROFILES_DIR.mkdir(parents=True, exist_ok=True)
 
     parser = get_parser()
     parse_args(parser)
