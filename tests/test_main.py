1b7d8967bfc13881f4ab452f90f01a1d3cf69dba
Vitor Jeronimo <vitor.bently@hotmail.com>
2023-06-15 08:20:59 -0300

    Created empty test file

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+from nexus.main import load_profile_config
+
+class TestMain(unittest.TestCase):
+
+    def load_profile_config__should_return_config_dict__when_profile_exists_on_disk(self):
+        pass
+
+
+    def load_profile_config__should_raise_FileNotFoundError__when_no_profile_can_be_found(self):
+        pass
